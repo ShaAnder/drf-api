cd9516c3b10427f9b6b697a8763dd715b0743db7
shaAnder <shaunanderton1@gmail.com>
2025-03-12 11:55:36 +0000

    add filtering and searches to posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,25 +9,26 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     reaction_id = serializers.SerializerMethodField()
+    reaction_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                "Image size larger than 2MB!"
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                "Image width larger than 4096px"
+                'Image width larger than 4096px!'
             )
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                "Image height larger than 4096px"
-            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
     def get_reaction_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
@@ -42,5 +43,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'reaction_id',
+            'title', 'content', 'image', 'image_filter',
+            'reaction_id', 'reactions_count', 'comments_count',
         ]
