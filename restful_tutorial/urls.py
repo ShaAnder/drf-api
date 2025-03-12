63ddd517314d8581bc9511e8d6f4984dccda17ec
shaAnder <shaunanderton1@gmail.com>
2025-03-12 17:39:19 +0000

    add natural time for app times, front page message

--- restful_tutorial/urls.py
+++ restful_tutorial/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_route),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
