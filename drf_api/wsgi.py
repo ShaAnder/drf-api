d28761f3c2933e7c4bd0a8f3ae90a389abdc307f
shaAnder <shaunanderton1@gmail.com>
2025-03-12 19:14:35 +0000

    add environ variable for hosts

--- drf_api/wsgi.py
+++ drf_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for restful_tutorial project.
+WSGI config for drf_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restful_tutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api.settings')
 
 application = get_wsgi_application()
