bfb8433e67d212f940d54384b0b3c0103e66147d
shaAnder <shaunanderton1@gmail.com>
2025-03-12 12:39:00 +0000

    add filtering and auth packages

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from restful_tutorial.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -19,16 +20,23 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'reactions__owner__profile',
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
         'title',
     ]
     ordering_fields = [
-        'reaction_count',
+        'reactions_count',
         'comments_count',
         'reactions__created_at',
     ]
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
