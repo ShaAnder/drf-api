c9fc71f7eb696d0a61fa74a343a2dd6d3866131d
shaAnder <shaunanderton1@gmail.com>
2025-03-21 21:49:28 +0000

    fix JWT_AUTH settings reference

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,15 +1,11 @@
+from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
-
 
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to my drf API!"
+        "message": "Welcome to my DRF API!"
     })
 
 
@@ -17,22 +13,29 @@ def root_route(request):
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
+    
+    # Retrieve settings from REST_AUTH dictionary
+    jwt_auth_cookie = settings.REST_AUTH.get('JWT_AUTH_COOKIE', 'my-app-auth')
+    jwt_auth_refresh_cookie = settings.REST_AUTH.get('JWT_AUTH_REFRESH_COOKIE', 'my-refresh-token')
+    jwt_auth_samesite = settings.REST_AUTH.get('JWT_AUTH_SAMESITE', "None")
+    jwt_auth_secure = settings.REST_AUTH.get('JWT_AUTH_SECURE', True)
+
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key=jwt_auth_cookie,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=jwt_auth_samesite,
+        secure=jwt_auth_secure,
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key=jwt_auth_refresh_cookie,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=jwt_auth_samesite,
+        secure=jwt_auth_secure,
     )
     return response
