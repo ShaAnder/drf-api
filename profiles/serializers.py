f901651db01ea1fc1a8453d6df0e3d6f9280a40d
shaAnder <shaunanderton1@gmail.com>
2025-03-22 17:32:04 +0000

    add image field to api

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -12,15 +12,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     following_count = serializers.ReadOnlyField()
     image = serializers.SerializerMethodField()
 
-    def get_image(self, obj):
-        if obj.image:
-            image_url = obj.image.url
-            if image_url.startswith("/"):  # Ensure relative URLs get prefixed
-                return f"https://res.cloudinary.com/dbqlgz0og{image_url}"
-            return image_url
-        return "https://res.cloudinary.com/dbqlgz0og/image/upload/v1741184873/gipldxaberkg5nt7rp9z.jpg"
-
-
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -31,8 +22,12 @@ class ProfileSerializer(serializers.ModelSerializer):
             following = Follower.objects.filter(
                 owner=user, followed=obj.owner
             ).first()
-            # print(following)
             return following.id if following else None
+        return None
+
+    def get_image(self, obj):
+        if obj.image:
+            return self.context['request'].build_absolute_uri(obj.image.url)
         return None
 
     class Meta:
