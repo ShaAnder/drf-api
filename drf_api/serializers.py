92df98f04be8fcdc7966b4e1044d5bd5cc05ce37
shaAnder <shaunanderton1@gmail.com>
2025-03-22 17:49:17 +0000

    add image field to api

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -3,10 +3,16 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_id = serializers.ReadOnlyField(source='user_profile.id')  
+    profile_image = serializers.SerializerMethodField()  
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image'
         )
+
+    def get_profile_image(self, obj):
+        request = self.context.get('request')
+        if obj.user_profile.image:  
+            return request.build_absolute_uri(obj.user_profile.image.url)
+        return None  
--- profiles/models.py
+++ profiles/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_profile' 
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
